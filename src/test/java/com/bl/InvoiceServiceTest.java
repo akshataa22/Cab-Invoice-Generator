a31a06a5963ec7896b8eeb3347fa4c2bf2961853
akshataa22 <dhanawadeakshata53@gmail.com>
2023-12-28 22:13:47 +0530

    Welcome message

---
+++ src/test/java/com/bl/InvoiceServiceTest.java
@@ -0,0 +1,10 @@
+package com.bl;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class InvoiceServiceTest {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Cab Invoice Generator Program.");
+    }
+}
