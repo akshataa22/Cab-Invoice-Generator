5db3488953fae2ad6f12acc7526cbe009b3c3d24
akshataa22 <dhanawadeakshata53@gmail.com>
2023-12-28 22:20:47 +0530

    Calculating fare when given distance and time

--- src/main/java/com/bl/InvoiceGenerator.java
+++ src/main/java/com/bl/InvoiceGenerator.java
@@ -1,5 +1,15 @@
 package com.bl;
 
 public class InvoiceGenerator {
+    private static final int costPerTime = 1;
+    private static final double minimumCostPerKm = 10;
+    private static final double minimumFare = 5;
 
+    public double calculateFare(double distance, int time) {
+        double totalFare = distance * minimumCostPerKm + time * costPerTime;
+        if (totalFare < minimumFare) {
+            return minimumFare;
+        }
+        return totalFare;
+    }
 }
--- src/test/java/com/bl/InvoiceServiceTest.java
+++ src/test/java/com/bl/InvoiceServiceTest.java
@@ -4,7 +4,25 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class InvoiceServiceTest {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Cab Invoice Generator Program.");
+    @Test
+    public void givenDistanceAndTimeShouldReturnTotalFare() {
+        InvoiceGenerator ig = new InvoiceGenerator();
+        double distance = 2.0;
+        int time = 5;
+        double fare = ig.calculateFare(distance, time);
+        double expected = 25;
+        Assert.assertEquals(expected, fare, 0.0);
+        System.out.println("givenDistanceAndTimeShouldReturnTotalFare: " + fare);
+    }
+
+    @Test
+    public void givenLessDistanceAndTimeShouldReturnMinimumFare() {
+        InvoiceGenerator ig = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = ig.calculateFare(distance, time);
+        double expected = 5;
+        Assert.assertEquals(expected, fare, 0.0);
+        System.out.println("givenLessDistanceAndTimeShouldReturnMinimumFare: " + fare);
     }
 }
